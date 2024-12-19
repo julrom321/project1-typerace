a681d6c6577823ffd8d424897f073a8436c4f46a
julrom321 <julrom321@gmail.com>
2024-12-19 11:02:44 -0500

    secondcommit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 /*-------------------------------- Constants --------------------------------*/
 import{words} from "./randomwords.js"
 /*---------------------------- Variables (state) ----------------------------*/
-let rnum = Math.floor(Math.random() * 2000) + 1;
+let randomtext = `${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]}`
+let currentPlayer = "Player-1";
+let turn = null;
+let cursorPosition = 0;
 /*------------------------ Cached Element References ------------------------*/
 const players = document.querySelectorAll(".players")
 const p1 = document.querySelector("#p1")
@@ -12,18 +15,11 @@ const p2WPM = document.querySelector("#p2-wpm")
 const dlog = document.querySelectorAll(".dialogue")
 const p1dlog = document.querySelector("#p1-dialogue")
 const p2dlog = document.querySelector("#p2-dialogue")
-const inputtype = document.querySelectorAll(".inputtype")
-const p1type = document.querySelector("#p1-type")
-const p2type = document.querySelector("#p2-type")
 /*-------------------------------- Functions --------------------------------*/
-function RandomDlog(){
-    p1dlog.textContent = `${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]}`
-    p2dlog.textContent = `${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]} ${words[Math.floor(Math.random() * 2000) + 1]}`
-}
-RandomDlog(); // function will get called when start button is pressed. but for testing purposes im leaving this on
 function CheckSpelling(){
-
+    
 }
+CheckSpelling() // function will be called when 
 function CountWPM(){
 
 }
@@ -37,3 +33,23 @@ function WinCondition(){
 
 }
 /*----------------------------- Event Listeners -----------------------------*/
+    const letters = randomtext.split("").map((letter) => {
+    const span = document.createElement("span");
+    span.innerText = letter;
+    p1dlog.appendChild(span); // p1dlog will be assigned to  its current player later, but for testing purposes its p1
+    return span;});
+    
+    let firstLetter = letters[0];
+    firstLetter.classList.add("cursor");
+    
+    document.addEventListener("keydown", ({key}) => {
+        if (key === firstLetter.innerText) {
+        firstLetter.classList.remove("cursor");
+        firstLetter.classList.add("done");
+        firstLetter = letters[++cursorPosition]
+        }})
+/*------------------------------- Grave yard --------------------------------*/
+// const inputtype = document.querySelectorAll(".inputtype")
+// const p1type = document.querySelector("#p1-type")
+// const p2type = document.querySelector("#p2-type")
+// // function RandomDlog(){} // function will get called when start button is pressed. but for testing purposes its on
